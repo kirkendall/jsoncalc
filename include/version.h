6ee7b0fe9ebe894098a99745926682ed2c690c08
Steve Kirkendall <kirkenda@gmail.com>
2025-04-12 20:32:40 -0700

    Changed the names of the version def's.

--- include/version.h
+++ include/version.h
@@ -1,4 +1,4 @@
-#define JCMAJOR 0
-#define JCMINOR 1
-#define JCVERSION "0.1"
-#define JCCOPYRIGHT "2024 by Steve Kirkendall"
+#define JSON_MAJOR 0
+#define JSON_MINOR 1
+#define JSON_VERSION "0.1"
+#define JSON_COPYRIGHT "\u00a9 2024 by Steve Kirkendall"
